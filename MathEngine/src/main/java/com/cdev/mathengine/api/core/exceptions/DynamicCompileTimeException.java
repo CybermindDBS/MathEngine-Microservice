4516590dd771e0f1ae7dd2fb896a638717f65b35
ShakthyBalan <shakthybalan.dbs@gmail.com>
2025-09-18 19:43:09 +0530

    Delete MathEngine directory

--- MathEngine/src/main/java/com/cdev/mathengine/api/core/exceptions/DynamicCompileTimeException.java
+++
@@ -1,7 +0,0 @@
-package com.cdev.mathengine.api.core.exceptions;
-
-public class DynamicCompileTimeException extends RuntimeException {
-    public DynamicCompileTimeException(Throwable cause) {
-        super(cause);
-    }
-}
--- MathEngine/src/main/java/com/cdev/mathengine/api/core/exceptions/InvalidExpressionException.java
+++
@@ -1,7 +0,0 @@
-package com.cdev.mathengine.api.core.exceptions;
-
-public class InvalidExpressionException extends RuntimeException {
-    public InvalidExpressionException(String message) {
-        super(message);
-    }
-}
