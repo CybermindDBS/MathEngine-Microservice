0eb04d165df2b3c9860386664ca021e26d63b6e3
ShakthyBalan <shakthybalan.dbs@gmail.com>
2025-09-18 19:44:26 +0530

    Delete MathEngineClient directory

--- MathEngineClient/src/main/resources/static/js/calc-selector.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.custom-textarea3').forEach(textarea => {
-        textarea.addEventListener('click', () => {
-            const calcId = textarea.getAttribute('data-id');
-            document.getElementById('cald-id-field').value = calcId;
-            document.getElementById('calculation-selection').submit();
-        });
-    });
-});
